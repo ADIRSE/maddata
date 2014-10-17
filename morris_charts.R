bb550d8ac6272521f1316cf2a915bb03e5583e8f
miguelfg <miguel.fiandor.gutierrez@gmail.com>
2014-10-17 18:37:24 +0200

    blabla

---
+++ morris_charts.R
@@ -0,0 +1,8 @@
+getSeriesChart <- function () {
+  data(economics, package = 'ggplot2')
+  econ <- transform(economics, date = as.character(date))
+  m1 <- mPlot(x = 'date', y = c('psavert', 'uempmed'), type = 'Line',
+              data = econ)
+  m1$set(pointSize = 0, lineWidth = 1)
+  m1
+}
